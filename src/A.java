849637626d1ea8b2b4a7a0dcc081d8db1b8d1f07
Pedro Agnaldo Zacarias Amancio <pepagnaldo@hotmail.com>
2023-09-30 21:59:50 -0300

    criação classes

--- src/A.java
+++ src/A.java
@@ -1,5 +1,40 @@
 public class A {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    private int A1;
+    private float A2;
+
+
+    public int getA1() {
+        return A1;
     }
+
+    
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+    
+    public float getA2() {
+        return A2;
+    }
+
+    
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+    public static void MA1(){
+        System.out.println("MA1()");
+    }
+
+    public static void MA2(){
+        System.out.println("MA2()");
+    }
+
+    public static void main(String[] args) throws Exception {
+        MA1();
+        MA2();
+    }
+
 }
+
+
--- src/B.java
+++ src/B.java
@@ -1,3 +1,38 @@
 public class B {
+    private int B1;
+    private float B2;
+
+
+    public int getB1() {
+        return B1;
+    }
+
     
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    
+    public float getB2() {
+        return B2;
+    }
+
+    
+    public void setA2(float B2) {
+        this.B2 = B2;
+    }
+
+    public static void MB1(){
+        System.out.println("MB1()");
+    }
+
+    public static void MB2(){
+        System.out.println("MB2()");
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        MB1();
+        MB2();
+    }
 }
