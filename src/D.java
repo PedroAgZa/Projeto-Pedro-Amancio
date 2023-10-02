17db1a2b9e9ddfe865cf507fce127768383bb0fe
PedroAgZa <108290926+PedroAgZa@users.noreply.github.com>
2023-10-02 11:24:47 -0300

    Metodo MD4 criado

--- src/D.java
+++ src/D.java
@@ -28,8 +28,13 @@ public class D {
         System.out.println("MD2()");
     }
 
+    public static void MD4(){
+        System.out.println("MD4()");
+    }
+
     public static void main(String[] args) throws Exception {
         MD1();
         MD2();
+        MD4();
     }
 }
